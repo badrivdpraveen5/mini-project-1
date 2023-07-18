e3b6dddd628edae3b8c0e5332d283319a8a8b95a
Badri Venkata Durga Praveen <badrivdpraveen5@gmail.com>
2023-07-18 22:41:29 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title="calculator";
+  input:any='';
+  msg="";
+  clean(){
+    this.input='';
+  }
+  back(){
+     this.input=this.input.substring(0,this.input.length-1);
+  }
+  insert(k:any){
+    this.input=this.input+k;
+  }
+calculate(){
+  try{
+    this.input=eval(this.input);
+    this.msg="";
+  }
+  catch (error) {
+    this.msg="Invalid expression";
+}   
+}
+}
